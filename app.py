8e29a05d5dfce62e5bdb0fcde1fe0217c6625f32
root <root@kgcpm.c.sacred-buffer-185606.internal>
2017-12-23 06:13:25 +0000

    ok...

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(_name_)
 @app.route("/")
 def main():
-return "Welcomr to the world of attached bathrooms"
-
+return "Welcome to the world of attached bathrooms"
+@app.route("/hello")
+def hello():
+    return "Hello Pranathi, Sahithi"
 if __name__ == "__main__":
 app.run(host='0.0.0.0', port=int('8001')
